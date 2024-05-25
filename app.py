e28882a72cb2118237df3bf079d745d0c7212826
Alecxander Jamille Andaya <65760047+alecxanderBlade@users.noreply.github.com>
2024-05-25 17:29:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,28 @@
-# Q&A Chatbot
-#from langchain.llms import OpenAI
-
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()  
 
 import streamlit as st
 import os
-import pathlib
-import textwrap
 
 import google.generativeai as genai
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
-
 def get_gemini_response(question):
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(question)
     return response.text
 
-##initialize our streamlit app
+st.set_page_config(page_title="Q&A")
 
-st.set_page_config(page_title="Q&A Demo")
-
-st.header("Gemini Application")
+st.header("🛫TravelBot Travel Companion🛬")
 
 input=st.text_input("Where would youn like to travel?: ",key="input")
-act_txt=st.text_input("Can you tell me what activities your are going to be doing in: " + input, key="act_txt")
+act_txt=st.text_input("Can you tell me what activities your are going to be doing in " + input + "?", key="act_txt")
 budget_amount = st.selectbox(
-    'How much is your budget for this trip?',
+    'How much is your budget for this trip in Php?',
     (10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000))
 submit=st.button("Submit")
 
